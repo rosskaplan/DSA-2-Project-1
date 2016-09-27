1e3687a8840576de0b9a2287d2719ae3b665d2f2
Ross Kaplan <ross.kaplan96@gmail.com>
2016-09-27 17:43:00 -0400

    Fixing a small rehash bug where capacity was changed before entries cleared causing a segfault in specific conditions

---
+++ hash.h
@@ -0,0 +1,48 @@
+#ifndef _HASH_H
+#define _HASH_H
+
+#include <vector>
+#include <string>
+
+class hashTable {
+
+ public:
+
+  hashTable(int size = 0);
+
+  int insert(const std::string &key, void *pv = NULL);
+
+  bool contains(const std::string &key);
+
+  //void *getPointer(const std::string &key, bool *b = NULL);
+
+  //int setPointer(const std::string &key, void *pv);
+
+  //bool remove(const std::string &key);
+
+ private:
+
+  class hashItem {
+
+  public:
+    std::string key;
+    bool isOccupied;
+    bool isDeleted;
+    void *pv;
+  };
+
+  int capacity; // The current capacity of the hash table.
+  int filled; // Number of occupied items in the table.
+
+  std::vector<hashItem> data; // The actual entries are here.
+
+  int hash(const std::string &key);
+
+  int findPos(const std::string &key);
+
+  bool rehash();
+
+  static unsigned int getPrime(int size);
+};
+
+#endif //_HASH_H
---
+++ spellcheck.cpp
@@ -0,0 +1,152 @@
+/* 
+ * Professor: Prof. Sable
+ * Student: Ross Kaplan
+ * Course: DSA 2
+ * Semester: Fall Semester 2016
+ * Program 1: Hashtable Dictionary
+ * Goal: Implement a dictionary using a hashtable to quickly check if all words in a file are valid.  If a word is invalid, it'll print to the output file provided with all of the words that are over 20 characters or are not in the dictionary.  Note: Numbers mean a word is ignored and apostrophes and dashes are part of words.  All other symbols separate words.
+*/
+
+#include <iostream>
+#include "hash.h"
+#include <cmath>
+#include <ctime>
+#include <string>
+#include <cstdlib>
+#include <sstream>
+#include <fstream>
+
+using namespace std;
+
+void loadDictionary(hashTable &); //prompt for dictionary file, read input file, and load it
+
+void spellCheckDoc(hashTable &); //prompt for doc to be spell checked and output file
+                                 //compute the output and send it out
+
+string wordToLower(string); //make word lowercase to store in hashtable
+
+string processInput(hashTable &, string, int);
+
+int main() {
+
+    clock_t clock1, clock2;
+    hashTable hash_table(10000);
+    clock1 = clock();
+    loadDictionary(hash_table);
+    clock2 = clock(); 
+    long double diff_time = double(clock2 - clock1) / CLOCKS_PER_SEC;
+    cout << "Loading of dictionary took " << diff_time << " seconds." << endl;
+    
+    clock1 = clock();
+    spellCheckDoc(hash_table);
+    clock2 = clock();
+    diff_time = double(clock2 - clock1) / CLOCKS_PER_SEC;
+    cout << "Spell checking of document took " << diff_time << " seconds." << endl;
+
+return 0;
+
+}
+
+void loadDictionary(hashTable &hash_table) {
+
+    string dict_file;
+    cout << "Dictionary file: " << endl;
+    cin >> dict_file;
+
+    ifstream dict (dict_file.c_str());
+    
+    if (dict.fail()) {
+        cerr << "Failed to open " << dict_file << endl;
+        exit(-1);
+    }
+    
+    string temp_word;
+    int i = 0;
+    while (dict >> temp_word) {
+        if (hash_table.insert(wordToLower(temp_word)) == -1) {
+            cerr << "Memory allocation issues!" << endl;
+            exit(-1);
+        }
+        i++; 
+    }
+
+    return;
+
+}
+
+string wordToLower(string temp_word) {
+
+    for (int i = 0; i < temp_word.length(); i++) {
+        temp_word[i] = tolower(temp_word[i]); //Loop through, set all characters to lowercase
+    }
+return temp_word;
+}
+
+void spellCheckDoc(hashTable &hash_table) {
+
+    string spell_check_doc;
+    cout << "Document to spell check: " << endl;
+    cin >> spell_check_doc;
+
+    string output_doc;
+    cout << "Document to output to: " << endl;
+    cin >> output_doc;
+
+    ifstream infile (spell_check_doc.c_str());
+    ofstream outfile (output_doc.c_str());
+    if (infile.fail()) {
+        cerr << "Failed to open for reading: " << spell_check_doc << endl;
+        exit(-1);
+    }
+
+    if (outfile.fail()) {
+        cerr << "Failed to open for writing: " << output_doc << endl;
+        exit(-1);
+    }
+
+    string temp_word, processed;
+    int line_number = 1;
+    char c;
+    while (infile.get(c)) {
+        if (isalnum(c) || (c == '-') || (c == '\'')) { //If character isn't a word separator
+            temp_word += tolower(c);
+            continue;
+        }
+
+        if (c == '\n' && temp_word == "") {
+            line_number++;
+            temp_word = "";
+            continue;
+        }
+
+        if (temp_word != "") {
+            processed = processInput(hash_table, temp_word, line_number);
+
+            
+            if (processed != "")
+                outfile << processed << endl; 
+        }
+        temp_word = "";
+
+        if (c == '\n')
+            line_number++; 
+    }
+}
+
+string processInput(hashTable &hash_table, string temp_word, int line_number) {
+    for (int i = 0; i < temp_word.length(); i++) {
+        if (isdigit(temp_word[i])) 
+            return "";
+    }
+    if (temp_word.length() > 20) {
+        return "Long word at line " + to_string(line_number) + ", starts: " + temp_word.substr(0,20);
+    }
+    if (hash_table.contains(temp_word)) {
+        return ""; 
+    }
+    if (!hash_table.contains(temp_word)) {
+        return "Unknown word at line " + to_string(line_number) + ": " + temp_word;
+    } else {
+        return "";
+    }
+}
